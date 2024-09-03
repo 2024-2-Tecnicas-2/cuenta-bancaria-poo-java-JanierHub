66e2c93baf5e6b638100c19526fb374cfb296ea6
Janier <janierf.quinonesq@konradlorenz.edu.co>
2024-09-03 15:33:17 -0500

    Primer prueba unitaria, calculando el tipo de interes

--- src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
@@ -2,6 +2,8 @@ package com.mycompany.cuenta.bancaria.poo;
 
 public class Principal {
     public static void main(String[] args) {
-        // TODO: Adiciona aquí el código que deseas para la Cuenta Bancaria.
+        CuentaBancaria cuenta = new CuentaBancaria("Janier","34",30.000);
+        
+       cuenta.CalcularInteres();
     }
 }
